ff74fe7ae83ae27aa0996541ae5d4dce31ca13b8
HarishBabu-DV <harishbabudv2002@gmail.com>
2025-02-21 19:01:42 +0530

    Created individual products page

--- src/pages/View.jsx
+++ src/pages/View.jsx
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-
+import { Skeleton } from '../components/ui/skeleton'
 const View = () => {
   const {id}=useParams()
   console.log(id);
-  
+  const [product,setProduct]=useState([])
+  const getProductData=async ()=>{
+    const res=await fetch(`https://dummyjson.com/products/${id}`)
+    const data=await res.json()
+    console.log(data);
+    setProduct(data)
+  }
+  useEffect(()=>{
+    getProductData()
+  })
   return (
-    <div>View</div>
+    <section>
+      {
+        product ?
+         <h1> {product.title} </h1> 
+        : <div className="space-y-2">
+            <Skeleton className="h-4 w-[300px]" />
+            <Skeleton className="h-4 w-[250px]" />
+            <Skeleton className="h-4 w-[200px]" />
+        </div>
+      }
+    </section>
   )
 }
 
