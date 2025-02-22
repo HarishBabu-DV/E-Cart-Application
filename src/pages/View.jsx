036194243b166fad51ea83675112a228e050e971
HarishBabu-DV <harishbabudv2002@gmail.com>
2025-02-22 22:11:42 +0530

    installed breadcrumbs

--- src/pages/View.jsx
+++ src/pages/View.jsx
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { Link, useLocation, useParams } from 'react-router-dom'
 import { Skeleton } from '../components/ui/skeleton'
 const View = () => {
+  const path=[]
+  const {pathname} =useLocation()
+  path=pathname.split("/")
   const {id}=useParams()
   console.log(id);
   const [product,setProduct]=useState([])
@@ -16,58 +19,14 @@ const View = () => {
   })
   return (  
     <section>
+      {/* Breadcrumbs  */}
       <div className="breadcrumbs text-sm">
-  <ul>
-    <li>
-      <a>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          className="h-4 w-4 stroke-current">
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"></path>
-        </svg>
-        Home
-      </a>
-    </li>
-    <li>
-      <a>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          className="h-4 w-4 stroke-current">
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"></path>
-        </svg>
-        Documents
-      </a>
-    </li>
-    <li>
-      <span className="inline-flex items-center gap-2">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          className="h-4 w-4 stroke-current">
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M9 13h6m-3-3v6m5 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"></path>
-        </svg>
-        Add Document
-      </span>
-    </li>
-  </ul>
-</div>
+        <ul>
+          <li><Link to={"/"}>Home</Link> </li>
+          <li><Link to={"/view"}>{path[0]}</Link> </li>
+          <li>{path[1]}</li>
+        </ul>
+      </div>
       {
         product ?
          <h1 className='font-semibold text-4xl'> {product.title} </h1> 
